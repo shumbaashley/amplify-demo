108e90d52e52c7798856499568060ce3b8f92fb6
Ashley Shumba <ashleytshumba@gmail.com>
2023-11-02 15:18:14 +0200

    testing 1-2

--- amplify/backend/function/amplifydemof23f2c7a/src/index.py
+++ amplify/backend/function/amplifydemof23f2c7a/src/index.py
@@ -11,5 +11,5 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+      'body': json.dumps('Hello from your new Amplify Python lambda for Ashley!')
   }

13df65338143c371053c6981f545a64a349c402a
Ashley Shumba <ashleytshumba@gmail.com>
2023-11-02 15:41:09 +0200

    update lambda function

--- amplify/backend/function/amplifydemof23f2c7a/src/index.py
+++ amplify/backend/function/amplifydemof23f2c7a/src/index.py
@@ -1,9 +1,14 @@
 import json
+import datetime
+
 
 def handler(event, context):
   print('received event:')
   print(event)
-  
+  current_time = datetime.datetime.now().time()
+  body = {
+      'message': 'Hello, the current time is ' + str(current_time)
+  }
   return {
       'statusCode': 200,
       'headers': {
@@ -11,5 +16,5 @@ def handler(event, context):
           'Access-Control-Allow-Origin': '*',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': json.dumps('Hello from your new Amplify Python lambda for Ashley!')
+      'body': json.dumps(body)
   }
